d6d5f84d6ebd247c5d04429ecfc6e315609dd5c6
MemersonSt <emerson.cajape@tecsu.edu.ec>
2023-12-05 22:30:27 -0500

    creando el modelo base

---
+++ apps/base/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+
+# Base model
+class BaseModel(models.Model):
+    id = models.AutoField(primary_key = True)
+    state = models.BooleanField('Estado', default=True)
+    #create_date = models.DateField(auto_now_add=True, default=timezone.now)
+    create_date = models.DateTimeField('Fecha de creación', auto_now=True, auto_now_add=False)
+    modified_date = models.DateField('Fecha de modificación', auto_now=True, auto_now_add=False)
+    delete_date = models.DateField('Fecha de eliminación', auto_now=True, auto_now_add=False)
+
+
+
+    class Meta:
+        abstract = True
+        verbose_name = 'BaseModel'
+        verbose_name_plural = 'BaseModels'
+
+
+
+
+    
