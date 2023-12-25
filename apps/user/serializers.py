623f6fac560716a07b86fb1a675329afd4b7d06b
MemersonSt <emerson.cajape@tecsu.edu.ec>
2023-12-25 14:36:22 -0500

    commit

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -1,31 +1,49 @@
 from rest_framework import serializers
 from .models import User
 
+
+class UserTokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'name',
+            'last_name'
+        )
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
 
-    #Una funciona que encripta las constraseñas y nos retorna un usuario con la constraseña encriptada
+    # Una funciona que encripta las constraseñas y nos retorna un usuario con la constraseña encriptada
     def create(self, validated_data):
         user = User(**validated_data)
         user.set_password(validated_data['password'])
         user.save()
         return user
-    #Para actualizar password
+
+    # Para actualizar password
+
     def update(self, instance, validated_data):
         update_user = super().update(instance, validated_data)
         update_user.set_password(validated_data['password'])
         update_user.save()
         return update_user
+
+
 class UserListSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-#Una funcion que se llama para listar mostrando solo datos seleccionados en vez de todos
+
+    # Una funcion que se llama para listar mostrando solo datos seleccionados en vez de todos
+
     def to_representation(self, instance):
         return {
-            'id': instance['id'], #se accede a la clave del valor con ['valor requerido']
-            #se puede cambiar el username a usuario sin necesidad cambiar el original
+            'id': instance['id'],  # se accede a la clave del valor con ['valor requerido']
+            # se puede cambiar el username a usuario sin necesidad cambiar el original
             'username': instance['username'],
             'email': instance['email'],
             'password': instance['password']
@@ -51,4 +69,3 @@ class UserListSerializer(serializers.ModelSerializer):
 #     def validate(self, data):
 #         print("validacion general")
 #         return data
-
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,22 +1,33 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status #Documentacion code status
-from .models import User
-from .serializers import UserSerializer, UserListSerializer
+from rest_framework import status  # Documentacion code status
+from apps.user.serializers import UserTokenSerializer
+from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
+
 
 # Create your views here.
 class Login(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
-        login_serializer = self.serializer_class(data = request.data, context={'request':request}) # Un serializador ya definido en ObtainAuthToken. user y password
+        login_serializer = self.serializer_class(data=request.data, context={
+            'request': request})  # Un serializador ya definido en ObtainAuthToken. user y password
         if login_serializer.is_valid():
-            print("paso validación")
-        return Response({'message':'hola desde response'}, status = status.HTTP_200_OK)
-
-
-
-
-
+            user = login_serializer.validated_data['user']  # obtenemos el usuario
+            print(user)
+            if user.is_active:
+                token, created = Token.objects.get_or_create(user=user)
+                user_serializer = UserTokenSerializer(user)
+                if created:
+                    return Response({
+                        'token': token.key,
+                        'user': user_serializer.data,
+                        'message': 'Inicio de sesion exitoso'
+                    }, status=status.HTTP_201_CREATED)
+            else:
+                return Response({'error': 'Este usuario no esta activo'}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({'error': 'Usuario o contraseña incorrectos'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message':'Hola desde response'}, status = status.HTTP_200_OK)
 # @api_view(['GET', 'POST'])
 # def user_api_view(request):
 #     if request.method == 'GET':
